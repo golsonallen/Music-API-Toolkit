04fee88e2bb3efbf10c6a00127fca130cdac928b
Griffin <griffino@umich.edu>
2020-06-09 00:12:02 -0400

    Couldn't get Selenium to work. Frame issue

--- converter.py
+++ converter.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from selenium import webdriver
+import time
 
 #authenticate requests
 def get_token():
@@ -24,12 +26,45 @@ def get_song_name():
     headers = {"Authorization": "Bearer " + token}
     song_response = requests.request(method = "GET", url = url, headers = headers)
     song_title = song_response.json()["name"]
-    song_popularity = song_response.json()["popularity"]
-    return song_title, song_popularity
+    return song_title
 
-def open_song():
-    import os
-    os.system("open \"\" https://music.apple.com/us/browse")
+class AppleMusic:
 
-print(get_song_name())
-#open_song()
+    def __init__(self):
+        self.driver = webdriver.Safari()
+        #pause to give the site time to log me in
+        
+    def open_apple_music(self):
+        self.driver.get("https://music.apple.com/browse")
+
+    def login(self):
+        login_buttom = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/button')
+        login_buttom.click()
+        time.sleep(20)
+
+        # Switch frame by id
+        self.driver.switch_to.frame((self.driver.find_elements_by_tag_name("iframe")[1]))
+
+        # Now click on button
+        soundcloud_id_sign_in = self.driver.find_element_by_xpath('//*[@id="account_name_text_field"]')
+        soundcloud_id_sign_in.send_keys("golsonallen@yahoo.com")
+
+        #apple_id_sign_in = self.driver.find_element_by_xpath('//*[@id="sign_in_up_email"]')
+        #apple_id_sign_in.send_keys("golsonallen@yahoo.com")
+        #sign_in_buttom = self.driver.find_element_by_xpath('//*[@id="sign-in"]')
+        #sign_in_buttom.click()
+
+    def search_song_title(self):
+        pass
+
+    def play_song(self):
+        pass
+
+    def main(self):
+        self.open_apple_music()
+        time.sleep(12)
+        self.login()
+
+#print(get_song_name())
+converter = AppleMusic()
+converter.main()
